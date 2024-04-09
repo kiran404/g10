142e05ad3b19ba74d9d9ac7c5f27de1e5674e26e
Kiran Mushyakhwa <mushyakhwa@gmail.com>
2024-04-09 17:36:23 -0400

    cors re arranged

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const createError = require("http-errors");
-const cors = require("cors");
+
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const appRouter = require("./routes");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
-app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-const appRouter = require("./routes");
+app.use(cors());
 
 app.get("/", function (req, res, next) {
   res.send("WORKING FINE");
